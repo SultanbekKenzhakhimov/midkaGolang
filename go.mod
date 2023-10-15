e5f577307ed6c5bf321dd3a5e1e3e5e5dfccd6cb
Sultanbek <skenzhahimov@gmail.com>
2023-10-16 00:24:49 +0600

    upd

--- go.mod
+++ go.mod
@@ -2,9 +2,18 @@ module midkaGolang
 
 go 1.21
 
-require gorm.io/gorm v1.25.5
+require (
+	github.com/gorilla/mux v1.8.0
+	gorm.io/driver/postgres v1.5.3
+	gorm.io/gorm v1.25.5
+)
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 )
--- main/main.go
+++ main/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"midkaGolang/handlers"
+)
+
+func main() {
+	dsn := "user=postgres password=postgres dbname=midkaDB port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handlers.InitDB(db)
+
+	router := mux.NewRouter()
+
+	// Define API routes for Power Tools
+	router.HandleFunc("/powerTools", handlers.GetAllPowerTools).Methods("GET")
+	router.HandleFunc("/powerTools", handlers.CreatePowerTool).Methods("POST")
+
+	// Start the server
+	fmt.Println("Server is running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
