d7e4601ba60196a71525bd54bdc1d02003629025
Sultanbek <skenzhahimov@gmail.com>
2023-10-16 00:29:23 +0600

    updNew

--- main/main.go
+++ main/main.go
@@ -22,6 +22,26 @@ func main() {
 
 	router := mux.NewRouter()
 
+	// Define API routes for Power Tools
+	router.HandleFunc("/powerTools", handlers.GetAllPowerTools).Methods("GET")
+	router.HandleFunc("/powerTools", handlers.CreatePowerTool).Methods("POST")
+
+	// Paints
+	router.HandleFunc("/paints", handlers.GetAllPaints).Methods("GET")
+	router.HandleFunc("/paints", handlers.CreatePaint).Methods("POST")
+
+	// Nail Screws
+	router.HandleFunc("/nailScrews", handlers.GetAllNailScrews).Methods("GET")
+	router.HandleFunc("/nailScrews", handlers.CreateNailScrew).Methods("POST")
+
+	// Plumbing Supplies
+	router.HandleFunc("/plumbingSupplies", handlers.GetAllPlumbingSupplies).Methods("GET")
+	router.HandleFunc("/plumbingSupplies", handlers.CreatePlumbingSupply).Methods("POST")
+
+	// Electrical Fixtures
+	router.HandleFunc("/electricalFixtures", handlers.GetAllElectricalFixtures).Methods("GET")
+	router.HandleFunc("/electricalFixtures", handlers.CreateElectricalFixture).Methods("POST")
+
 	// Start the server
 	fmt.Println("Server is running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
